c26bb74de8f79a34278ab35b5cb32b6c26c58fc0
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2011-06-18 03:18:05 -0300

    vejamos

--- event.c
+++ event.c
@@ -48,38 +48,38 @@
 #include "types.h"
 #include "event.h"
 
-float vel = 250;                 /* velocidade linear */
-float ang_vel = 0.2 ;            /* velocidade angular */
+float vel = 250;                 /* velocidade linear       */
+float ang_vel = 0.2 ;            /* velocidade angular      */
 
-float theta = -271;              /* orientacao no plano xz */
-float phi = 0;                   /* orientacao no plano xy */
+float theta = -271;              /* orientacao no plano xz  */
+float phi = 0;                   /* orientacao no plano xy  */
 
-vec3 mypos =                     /* posicao do personagem */
+vec3 mypos =                     /* posicao do personagem   */
     { .x = 620, .y = 42, .z = 18 };
 
-float light[4] =                 /* posicao da luz 0 */
+float light[4] =                 /* posicao da luz 0        */
     { 0, 700, 0, 1 };
-char stop_light = 0;             /* luz 0 esta parada? */
+char stop_light = 0;             /* luz 0 esta parada?      */
 
-float light_color[4] =           /* cor da luz 1 */
+float light_color[4] =           /* cor da luz 1            */
     { 0.5, 0.5, 0.5, 0 };
 
-float shininess = 2;           /* brilho do material */  
-float diffuse[4]=                /* reflectancia difusa */
+float shininess = 2;             /* brilho do material      */  
+float diffuse[4]=                /* reflectancia difusa     */
     { 10, 10, 10, 1 };
-float specular[4]=               /* reflectancia especular */
+float specular[4]=               /* reflectancia especular  */
     { 2, 2, 2, 1 };
 
-char key_pressed[512];           /* keymap continuo */
-char key_hit[512];               /* keymap toggle */
+char key_pressed[512];           /* keymap continuo         */
+char key_hit[512];               /* keymap toggle           */
 
-char use_texture = 1;
+char use_texture = 1;            /* modelo com textura?     */
 
-char hide_text = 1;              /* esconder texto */
-char grab = 1;                   /* se o mouse esta preso na janela */
+char hide_text = 1;              /* esconder texto          */
+char grab = 1;                   /* prender mouse na janela */
 
-char fps_str[8] = "O FPS";       /* fps na tela */
-char status_str[3][256];         /* variaveis na tela */
+char fps_str[8] = "O FPS";       /* fps na tela             */
+char status_str[3][256];         /* variaveis na tela       */
 
 void init_event_keys()
 {
